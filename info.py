10b2c27156ca79d1c52b2c2907d0713f9a510171
MODSQ1234 <90554612+MODSQ1234@users.noreply.github.com>
2024-02-15 19:20:22 +0200

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ ADMINS = 1098027220
 CHANNELS = -1001625446236
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = 100162544623
+auth_channel = auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
